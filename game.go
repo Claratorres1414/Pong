94a9bbd572c51f7e24d630b75475cb8ae9e5f5d0
Claratorres1414 <claratorres12345679@gmail.com>
2026-07-12 16:01:12 -0300

    feat: separa game do arquivo main

--- game.go
+++ game.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"image/color"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+type Game struct {
+	Player Paddle
+}
+
+func (g *Game) Update() error {
+	g.Player.Update()
+
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	screen.Fill(color.RGBA{
+		R: 25,
+		G: 25,
+		B: 35,
+		A: 255,
+	})
+
+	g.Player.Draw(screen)
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return ScreenWidth, ScreenHeight
+}
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
@@ -12,38 +11,9 @@ const (
 	ScreenHeight = 600
 )
 
-type Game struct {
-	Player Paddle
-}
-
-func (g *Game) Update() error {
-
-	return nil
-}
-
-func (g *Game) Draw(screen *ebiten.Image) {
-	screen.Fill(color.RGBA{
-		R: 25,
-		G: 25,
-		B: 35,
-		A: 255,
-	})
-
-	g.Player.Draw(screen)
-}
-
-func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
-	return ScreenWidth, ScreenHeight
-}
-
-func init() {
-	whiteImage = ebiten.NewImage(1, 1)
-	whiteImage.Fill(color.White)
-}
-
 func main() {
 	ebiten.SetWindowSize(ScreenWidth, ScreenHeight)
-	ebiten.SetWindowTitle("Pong em Go")
+	ebiten.SetWindowTitle("Pong")
 
 	game := &Game{
 		Player: Paddle{
