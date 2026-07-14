613ad1f27ad7426305787500da0f9a4691ac8bb1
Claratorres1414 <claratorres12345679@gmail.com>
2026-07-14 15:07:39 -0300

    feat: previne possibilidade de bugs nas colisões

--- ball.go
+++ ball.go
@@ -15,11 +15,23 @@ func (b *Ball) Update() {
 	b.X += b.VX
 	b.Y += b.VY
 
-	if b.Y <= 0 || b.Y+b.Height >= ScreenHeight {
+	if b.Y <= 0 {
+		b.Y = 0
 		b.VY *= -1
 	}
 
-	if b.X <= 0 || b.X+b.Width >= ScreenWidth {
+	if b.Y+b.Height >= ScreenHeight {
+		b.Y = ScreenHeight - b.Height
+		b.VY *= -1
+	}
+
+	if b.X <= 0 {
+		b.X = 0
+		b.VX *= -1
+	}
+
+	if b.X+b.Width >= ScreenWidth {
+		b.X = ScreenWidth - b.Width
 		b.VX *= -1
 	}
 }
